622ecd03c2378230d8488c67374061c3518ad29c
LnX <luan38.eric@gmail.com>
2021-12-10 14:33:54 -0300

    nova atualização

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ function animacaoScroll(){
     const sections = document.querySelectorAll('.js-scroll');
 
     if(sections.length){
-        const windowMetade = window.innerHeight * 0.5;
+        const windowMetade = window.innerHeight * 0.7;
 
         function animaScroll(){
             sections.forEach((e) => {
                 const sectionTop = e.getBoundingClientRect().top;
-                const isSectionVisible = (sectionTop - windowMetade);
+                const isSectionVisible = (sectionTop - windowMetade) < 0;
 
                 if(isSectionVisible){
                     e.classList.add('ativo');
@@ -15,6 +15,7 @@ function animacaoScroll(){
 
             })
         }
+        animaScroll();
         window.addEventListener('scroll', animaScroll);
     }
 }
