8b54c7dd8076e8c0b61b96e6fc907e5e2a00e2cb
LarissaDezuani <dezuanilariss@gmail.com>
2022-01-10 02:25:09 -0300

    implementando paginas da parte visual no c# e arquivos der e uml

--- c#/CorDosOlhos/Controllers/HomeController.cs
+++ c#/CorDosOlhos/Controllers/HomeController.cs
@@ -27,7 +27,10 @@ namespace CorDosOlhos.Controllers
         {
             return View();
         }
-
+        public IActionResult Sobre()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
